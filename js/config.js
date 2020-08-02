13eaa890754ae7857455c085f9818460153de8df
yike5460 <yike5460@163.com>
2020-08-02 22:22:06 +0800

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_vThcP2Kvj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '58dj899v8o0p0rari7f3s9d60s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
